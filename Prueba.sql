1f78c828dd187f457abd892e54b972424d1324ab
Inigo Ormaechea <inigo.ormaechea@arin-innovation.com>
2016-12-06 17:29:19 +0100

    Commit Tras Reset

--- Prueba.sql
+++ Prueba.sql
@@ -1,1 +1,11 @@
 select sysdate from dual;
+
+--First Modification
+select * from fnd_product_groups;
+
+--Second Modificacion
+select * from v$instance;
+
+--Nuevo comentario
+--Otro Comentario
+ --New comment tras reset hard
